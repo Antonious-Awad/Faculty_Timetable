36f8d773624d792fcb28d589c52990151425bb11
Antonious-Awad <antonuostony1@gmail.com>
2021-12-18 19:46:15 +0200

    update

--- GA.py
+++ GA.py
@@ -9,30 +9,30 @@ mutation_rate = 0.01
 cross_rate = 0.1  # number of retained population from last generation
 
 
-
 class GeneticAlgorithm:
 
     def _tournament_selection(self, population):
-        tournament_population = p.Population( 0 )
-        for _ in range( k ):
+        tournament_population = p.Population(0)
+        for _ in range(k):
             tournament_population.get_schedules().append(
-                population.get_schedules()[random.randrange( 0, POPULATION_NUM )] )
-        tournament_population.get_schedules().sort( key=lambda x: x.get_fitness() )
+                population.get_schedules()[random.randrange(0, POPULATION_NUM)])
+        tournament_population.get_schedules().sort(key=lambda x: x.get_fitness())
         return tournament_population.get_schedules()[0]
 
     def _crossover_population(self, population):
-        crossover_population = p.Population( 0 )
-        elite_count = round( len( population.get_schedules() ) * cross_rate )
-        crossover_population.get_schedules().append( population.get_schedules()[:elite_count] )
-        for _ in range( elite_count, POPULATION_NUM ):
-            sched1 = self._tournament_selection( population )
-            sched2 = self._tournament_selection( population )
-            crossover_population.get_schedules().append( self._crossover( sched1, sched2 ) )
+        crossover_population = p.Population(0)
+        elite_count = round(len(population.get_schedules()) * cross_rate)
+        crossover_population.get_schedules().append(
+            population.get_schedules()[:elite_count])
+        for _ in range(elite_count, POPULATION_NUM):
+            sched1 = self._tournament_selection(population)
+            sched2 = self._tournament_selection(population)
+            crossover_population.get_schedules().append(self._crossover(sched1, sched2))
         return crossover_population
 
     def _crossover(self, parent1, parent2):
         child = s.Schedule().initialization()
-        for i in range( len( child.get_classes() ) ):
+        for i in range(len(child.get_classes())):
             if random.random() > 0.5:
                 child.get_classes()[i] = parent1.get_classes()[i]
             else:
@@ -40,14 +40,14 @@ class GeneticAlgorithm:
         return child
 
     def _mutate_population(self, population):
-        elite_count = round( len( population.get_schedules() ) * cross_rate )
-        for i in range( elite_count, POPULATION_NUM ):
-            self._mutate( population.get_schedules()[i] )
+        elite_count = round(len(population.get_schedules()) * cross_rate)
+        for i in range(elite_count, POPULATION_NUM):
+            self._mutate(population.get_schedules()[i])
         return population
 
     def _mutate(self, schedule):
         mutated_schedule = s.Schedule().initialization()
-        for i in range( len( mutated_schedule.get_classes() ) ):
+        for i in range(len(mutated_schedule.get_classes())):
             if (mutation_rate > random.random()):
                 mutated_schedule.get_classes()[i] = schedule.get_classes()[i]
         return mutated_schedule
