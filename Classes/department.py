995327b430f02cd9cbd297ca35cfeb721633bd53
Antonious-Awad <antonuostony1@gmail.com>
2021-12-18 19:29:10 +0200

    delete

--- Classes/department.py
+++
@@ -1,5 +0,0 @@
-class Department:
-    def __init__(self, name):
-        self._name = name
-
-    def get_name(self): return self._name
--- Classes/instructor.py
+++
@@ -1,8 +0,0 @@
-class Instructor:
-    def _init_(self,instructor_name,instructor_number):
-        self.instructor_name = instructor_name
-        self.instructor_number = instructor_number
-
-    def get_instructor(self):
-
-       return "instructor:" + self.instructor_name + ", instructor number:" + self.instructor_number    
