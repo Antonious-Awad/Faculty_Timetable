8bc5b92b9f34333caa7bad90eb9b7a2a02ad872e
Antonious-Awad <antonuostony1@gmail.com>
2021-12-19 00:03:34 +0200

    delete

--- Classes/Student.py
+++
@@ -1,7 +0,0 @@
-class Student:
-    def __init__(self, id, name):
-        self._id = id
-        self._name = name
-
-    def get_id(self): return self._id
-    def get_name(self): return self._name
